8f137831e4acf72ba5cfbbfce1f19d40e8dac829
atom --wait <YOUR EMAIL ADDRESS>
2016-06-30 13:19:49 -0400

    day2 hw

---
+++ js/main.js
@@ -0,0 +1,124 @@
+
+    var goodMovie = [],
+        rllyGoodMovie = []
+
+        movie1 = {
+            title: 'Apocalypse Now',
+            genre: 'Drama',
+            year: 1979,
+            actors: ['Martin Sheen', 'Marlon Brando', 'Robert Duvall'],
+            stars: 8.5
+        }
+        movie2 = {
+            title: 'Microcosmos',
+            genre: 'Documentary',
+            year: 1996,
+            actors: ['Kristin Scott Thomas', 'Jacques Perrin'],
+            stars: 7.9
+        }
+        movie3 = {
+            title: 'Akira',
+            genre: 'Animation',
+            year: 1988,
+            actors: ['Nozomu Sasaki', 'Mami Koyama', 'Mitsuo Iwata'],
+            stars: 8.1
+        }
+        movie4 = {
+            title: 'Ghost in the Shell',
+            genre: 'Animation',
+            year: 1995,
+            actors: ['Atsuko Tanaka', 'Iemasa Kayumi', 'Akio Otsuka'],
+            stars: 8.0
+        }
+        movie5 = {
+            title: 'Perfect Blue',
+            genre: 'Animation',
+            year: 1997,
+            actors: ['Junko Iwao', 'Rica Matsumoto', 'Shinpachi Tsuji'],
+            stars: 7.8
+        }
+
+        goodMovie.push(movie1)
+        goodMovie.push(movie2)
+        goodMovie.push(movie3)
+        goodMovie.push(movie4)
+        goodMovie.push(movie5)
+
+        console.log(goodMovie)
+
+        goodMovie.sort(function (a, b) {
+          if (a.title > b.title) {
+            return 1;
+          }
+          if (a.title < b.title) {
+            return -1;
+          }
+          return 0;
+        })
+
+        goodMovie.sort(function (a, b) {
+          if (a.stars > b.stars) {
+            return 1;
+          }
+          if (a.stars < b.stars) {
+            return -1;
+          }
+          return 0;
+        })
+
+        // goodMovie = JSON.stringify(goodMovie);
+        // console.log(goodMovie);
+
+        // title
+        // [{"title":"Akira","genre":"Animation","year":1988,"actors":["Nozomu Sasaki","Mami Koyama","Mitsuo Iwata"],"stars":8.1},{"title":"Apocalypse Now","genre":"Drama","year":1979,"actors":["Martin Sheen","Marlon Brando","Robert Duvall"],"stars":8.5},{"title":"Ghost in the Shell","genre":"Animation","year":1995,"actors":["Atsuko Tanaka","Iemasa Kayumi","Akio Otsuka"],"stars":8},{"title":"Microcosmos","genre":"Documentary","year":1996,"actors":["Kristin Scott Thomas","Jacques Perrin"],"stars":7.9},{"title":"Perfect Blue","genre":"Animation","year":1997,"actors":["Junko Iwao","Rica Matsumoto","Shinpachi Tsuji"],"stars":7.8}]
+
+        // stars
+        // [{"title":"Perfect Blue","genre":"Animation","year":1997,"actors":["Junko Iwao","Rica Matsumoto","Shinpachi Tsuji"],"stars":7.8},{"title":"Microcosmos","genre":"Documentary","year":1996,"actors":["Kristin Scott Thomas","Jacques Perrin"],"stars":7.9},{"title":"Ghost in the Shell","genre":"Animation","year":1995,"actors":["Atsuko Tanaka","Iemasa Kayumi","Akio Otsuka"],"stars":8},{"title":"Akira","genre":"Animation","year":1988,"actors":["Nozomu Sasaki","Mami Koyama","Mitsuo Iwata"],"stars":8.1},{"title":"Apocalypse Now","genre":"Drama","year":1979,"actors":["Martin Sheen","Marlon Brando","Robert Duvall"],"stars":8.5}]
+
+        movie6 = {
+            title: 'The Scorpion King 2: Rise of a Warrior',
+            genre: 'Action',
+            year: 2008,
+            actors: ['michael Copon', 'Karen David', 'Simon Quarterman'],
+            stars: 3.8
+        }
+        movie7 = {
+            title: 'The Scorpion King 3: Battle for Redemption',
+            genre: 'Action',
+            year: 2012,
+            actors: ['Dave Bautista', 'Bostin Christopher', 'Tanapol Chuksrida'],
+            stars: 3.6
+        }
+        movie8 = {
+            title: 'The Scorpion King 4: Quest for Power',
+            genre: 'Action',
+            year: 2015,
+            actors: ['Victor Webster', 'Ellen Hollman', 'Will Kemp'],
+            stars: 4.3
+        }
+
+        rllyGoodMovie.push(movie6)
+        rllyGoodMovie.push(movie7)
+        rllyGoodMovie.push(movie8)
+
+        console.log(rllyGoodMovie)
+
+        rllyGoodMovie.sort(function (a, b) {
+          if (a.stars > b.stars) {
+            return 1;
+          }
+          if (a.stars < b.stars) {
+            return -1;
+          }
+          return 0;
+        })
+
+        var allMovie = goodMovie.concat(rllyGoodMovie)
+        console.log(allMovie)
+        // allMovie = JSON.stringify(allMovie)
+        // console.log(allMovie)
+        //
+        // // all
+        // // [{"title":"Perfect Blue","genre":"Animation","year":1997,"actors":["Junko Iwao","Rica Matsumoto","Shinpachi Tsuji"],"stars":7.8},{"title":"Microcosmos","genre":"Documentary","year":1996,"actors":["Kristin Scott Thomas","Jacques Perrin"],"stars":7.9},{"title":"Ghost in the Shell","genre":"Animation","year":1995,"actors":["Atsuko Tanaka","Iemasa Kayumi","Akio Otsuka"],"stars":8},{"title":"Akira","genre":"Animation","year":1988,"actors":["Nozomu Sasaki","Mami Koyama","Mitsuo Iwata"],"stars":8.1},{"title":"Apocalypse Now","genre":"Drama","year":1979,"actors":["Martin Sheen","Marlon Brando","Robert Duvall"],"stars":8.5},{"title":"The Scorpion King 3: Battle for Redemption","genre":"Action","year":2012,"actors":["Dave Bautista","Bostin Christopher","Tanapol Chuksrida"],"stars":3.6},{"title":"The Scorpion King 2: Rise of a Warrior","genre":"Action","year":2008,"actors":["michael Copon","Karen David","Simon Quarterman"],"stars":3.8},{"title":"The Scorpion King 4: Quest for Power","genre":"Action","year":2015,"actors":["Victor Webster","Ellen Hollman","Will Kemp"],"stars":4.3}]
+
+    
---
+++ js/notes1.js
@@ -0,0 +1,32 @@
+var movie = {
+  title: 'Selma',
+  genre: 'Historical',
+  year: 2015
+}
+
+movie = JSON.stringify(movie)
+
+console.log(movie)
+
+
+  //   var movie1 = {},
+  //       movie2 = {},
+  //       movie3 = {},
+  //       movie4 = {}
+  //       movie5 = {}
+  //
+  //   movie1.title = ''
+  //
+  //   var movies = []
+  //
+  //   var movie ={
+  //     title: 'Movie Title',
+  //     year: 2020,
+  //     genre: 'horror'
+  //   }
+  //
+  //   console.log(movies.push(movie))
+  //
+  //   movies.push(1)
+  //
+  // }
